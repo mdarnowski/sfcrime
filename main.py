a31dcf4a0545151bbf938c780f28d749b35cdf64
Zacer559 <zacer559@gmail.com>
2023-06-10 01:37:07 +0200

    Main is using SQLAlchemy very efficiently <1:30

--- main.py
+++ main.py
@@ -34,18 +34,21 @@ def get_mappings(session, df, table_class, key_column, *columns):
     """
     mapping = {}
     unique_df = df[list(columns)].drop_duplicates()
+
     for record in unique_df.values:
-        filter_conditions = {col: value for col, value in zip(columns, record)}
+        filter_conditions = dict(zip(columns, record))
         result = session.query(getattr(table_class, key_column)).filter_by(**filter_conditions).first()
 
         if result is None:
-            new_record = {col: value for col, value in zip(columns, record)}
+            new_record = dict(zip(columns, record))
             obj = table_class(**new_record)
             session.add(obj)
-            session.flush()
             mapping[tuple(record)] = getattr(obj, key_column)
         else:
             mapping[tuple(record)] = result[0]
+
+    # Flush once after all records have been added
+    session.flush()
 
     return mapping
 
@@ -78,47 +81,52 @@ def insert_data(df, engine):
     total_rows = df.shape[0]
     num_batches = -(-total_rows // batch_size)
 
+    # Precompile the column names
+    date_cols = ["incident_datetime", "incident_date", "incident_time", "incident_year", "incident_day_of_week", "report_datetime"]
+    location_cols = ["latitude", "longitude"]
+    incident_details_cols = ["incident_number", "incident_description"]
+
     for batch_num in tqdm(range(num_batches), desc="Inserting rows"):
         start_idx = batch_num * batch_size
         end_idx = min((batch_num + 1) * batch_size, total_rows)
         batch_df = df.iloc[start_idx:end_idx]
 
         # Prepare values for batch insertion
-        date_values = batch_df[
-            ['incident_datetime', 'incident_date', 'incident_time', 'incident_year', 'incident_day_of_week',
-             'report_datetime']].values.tolist()
-        location_values = batch_df[['latitude', 'longitude']].values.tolist()
-        incident_details_values = batch_df[['incident_number', 'incident_description']].values.tolist()
+        date_values = [dict(zip(date_cols, record)) for record in batch_df[date_cols].values.tolist()]
+        location_values = [dict(zip(location_cols, record)) for record in batch_df[location_cols].values.tolist()]
+        incident_details_values = [dict(zip(incident_details_cols, record)) for record in batch_df[incident_details_cols].values.tolist()]
 
-        # Insert batch and get the keys
-        date_keys = session.execute(
-            insert(DateDimension).values([dict(zip(
-                ["incident_datetime", "incident_date", "incident_time", "incident_year", "incident_day_of_week",
-                 "report_datetime"], record)) for record in date_values]).returning(DateDimension.date_key)
-        ).fetchall()
-        location_keys = session.execute(
-            insert(LocationDimension).values([dict(zip(["latitude", "longitude"], record)) for record in location_values]).returning(LocationDimension.location_key)
-        ).fetchall()
-        incident_detail_keys = session.execute(
-            insert(IncidentDetailsDimension).values([dict(zip(["incident_number", "incident_description"], record)) for record in incident_details_values]).returning(IncidentDetailsDimension.incident_details_key)
-        ).fetchall()
+        # Bulk insert
+        session.bulk_insert_mappings(DateDimension, date_values)
+        session.bulk_insert_mappings(LocationDimension, location_values)
+        session.bulk_insert_mappings(IncidentDetailsDimension, incident_details_values)
+
+        # Query the recently inserted IDs
+        date_keys = session.query(DateDimension.date_key).order_by(DateDimension.date_key.desc()).limit(len(date_values)).all()
+        location_keys = session.query(LocationDimension.location_key).order_by(LocationDimension.location_key.desc()).limit(len(location_values)).all()
+        incident_detail_keys = session.query(IncidentDetailsDimension.incident_details_key).order_by(IncidentDetailsDimension.incident_details_key.desc()).limit(len(incident_details_values)).all()
+
+        # Reverse the keys because we queried in descending order
+        date_keys = list(reversed(date_keys))
+        location_keys = list(reversed(location_keys))
+        incident_detail_keys = list(reversed(incident_detail_keys))
 
         # Prepare the final batch values for insertion
         batch_values = [
             {
                 'row_id': row.row_id,
                 'date_key': date_keys[idx][0],
-                'category_key': mappings['category_dimension'][(row.incident_category, row.incident_subcategory, row.incident_code)],
-                'district_key': mappings['district_dimension'][(row.police_district, row.analysis_neighborhood)],
-                'resolution_key': mappings['resolution_dimension'][(row.resolution,)],
+                'category_key': mappings['category_dimension'][tuple([row.incident_category, row.incident_subcategory, row.incident_code])],
+                'district_key': mappings['district_dimension'][tuple([row.police_district, row.analysis_neighborhood])],
+                'resolution_key': mappings['resolution_dimension'][tuple([row.resolution])],
                 'location_key': location_keys[idx][0],
                 'incident_details_key': incident_detail_keys[idx][0]
             }
             for idx, row in enumerate(batch_df.itertuples(index=False))
         ]
 
-        # Insert the final batch
-        session.execute(insert(Incidents), batch_values)
+        # Use bulk_insert_mappings for final batch insert
+        session.bulk_insert_mappings(Incidents, batch_values)
 
         # Commit the changes
         session.commit()
