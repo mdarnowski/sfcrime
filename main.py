67ae94f961366f3436f0cbeef89507d6b1b936b5
mdarnowski <s18322@pjwstk.edu.pl>
2023-06-19 17:43:31 +0200

    model refactor, added Dimension class, indexing and mapping fixes

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 import pandas as pd
 from tqdm import tqdm
-from sqlalchemy import create_engine, MetaData
-from sqlalchemy.orm import sessionmaker, registry, class_mapper
+
 from config.database import db_config
 from model.SQLAlchemy import DateDimension, CategoryDimension, DistrictDimension, IncidentDetailsDimension, \
     LocationDimension, ResolutionDimension, Incidents
@@ -15,152 +16,132 @@ def load_data(filepath):
     :return: DataFrame with loaded data.
     """
     df = pd.read_csv(filepath)
-    df.columns = df.columns.str.replace(' ', '_')
-    df.columns = map(str.lower, df.columns)
+    df.columns = df.columns.str.replace(' ', '_').str.lower()
     return df.where(pd.notnull(df), None)
 
 
-def get_mappings(session, df, table_class, key_column, *columns):
+def get_mappings(session, df, table_class):
     """
-    Get mappings for dimensions.
+    Generates a dictionary with mappings for the dimensions based on the data from a DataFrame and a table class.
 
-    :param session: SQLAlchemy session.
-    :param df: DataFrame containing the data.
-    :param table_class: ORM class for the table.
-    :param key_column: The key column in the table.
-    :param columns: Columns to be handled.
-    :return: A dictionary containing mappings for dimensions.
+    :param session: An active SQLAlchemy session.
+    :param df: The DataFrame containing the data.
+    :param table_class: The class of the table.
+    :return: A dictionary with mappings for the dimensions.
     """
     existing_records = session.query(table_class).all()
-    existing_mapping = {tuple(getattr(record, col) for col in columns): getattr(record, key_column) for record in
-                        existing_records}
-
+    columns = table_class.get_columns()
+    existing_mapping = {tuple(getattr(record, col) for col in columns): record.key for record in existing_records}
     unique_df = df[list(columns)].drop_duplicates()
-    new_records = [dict(zip(columns, row)) for row in unique_df.values if tuple(row) not in existing_mapping]
+    new_records = [
+        dict(zip(columns, row)) for row in unique_df.values
+        if tuple(row) not in existing_mapping and not all(pd.isnull(cell) for cell in row)
+    ]
 
     new_objects = [table_class(**record) for record in new_records]
     session.add_all(new_objects)
     session.commit()
 
-    new_mapping = {tuple(getattr(obj, col) for col in columns): getattr(obj, key_column) for obj in new_objects}
+    new_mapping = {tuple(getattr(obj, col) for col in columns): obj.key for obj in new_objects}
     mapping = {**existing_mapping, **new_mapping}
 
     return mapping
 
 
+def get_key(row, mapping, *columns):
+    """
+    Retrieve the key from a mapping using row values.
+
+    :param row: DataFrame row
+    :param mapping: dictionary with keys as tuples
+    :param columns: tuple with column names
+    :return: The key from the mapping if found, otherwise None
+    """
+    key_elements = tuple(getattr(row, col) for col in columns)
+    return mapping.get(key_elements)
+
+
+def get_keys(session, dimension_class, num_of_values):
+    """
+    Retrieves keys for a given dimension class and number of values.
+
+    :param session: An active SQLAlchemy session.
+    :param dimension_class: The class of the dimension table.
+    :param num_of_values: The number of values inserted into the dimension table.
+    :return: A range object representing the keys.
+    """
+    last_key = session.query(dimension_class.key).order_by(dimension_class.key.desc()).first()[0]
+    keys = range(last_key - num_of_values + 1, last_key + 1)
+    return keys
+
+
+def bulk_insert_and_get_keys(session, batch_df, dimension_class):
+    """
+    Performs bulk insertion and retrieves keys for a given batch DataFrame and dimension class.
+
+    :param session: An active SQLAlchemy session.
+    :param batch_df: A DataFrame representing a batch of data.
+    :param dimension_class: The class of the dimension table.
+    :return: A range object representing the keys.
+    """
+    values = batch_df[dimension_class.get_columns()].to_dict('records')
+    session.bulk_insert_mappings(dimension_class, values)
+    keys = get_keys(session, dimension_class, len(values))
+
+    return keys
+
+
 def insert_data(df, engine):
     """
-    Insert data into the database.  This function will insert data into the dimension tables first, then insert data
+    Insert data into the database. This function will insert data into the dimension tables first, then insert data
+    into the fact table.
+
     :param df: Dataframe containing the data.
     :param engine: Engine to connect to the database.
-    :return:    None
+    :return: None
     """
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Define mappings
     mappings = {
-        'district_dimension': get_mappings(
-            session, df, DistrictDimension, 'district_key', 'police_district', 'analysis_neighborhood'
-        ),
-        'resolution_dimension': get_mappings(
-            session, df, ResolutionDimension, 'resolution_key', 'resolution'
-        ),
-        'category_dimension': get_mappings(
-            session, df, CategoryDimension, 'category_key', 'incident_category', 'incident_subcategory', 'incident_code'
-        )
+        DimensionClass: get_mappings(session, df, DimensionClass)
+        for DimensionClass in [DistrictDimension, ResolutionDimension, CategoryDimension, LocationDimension]
     }
 
-    # Insert batches
     batch_size = 10000
-    total_rows = df.shape[0]
-    num_batches = -(-total_rows // batch_size)
+    for start_idx in tqdm(range(0, len(df), batch_size), desc="Inserting rows"):
+        batch_df = df.iloc[start_idx:start_idx + batch_size]
 
-    date_cols = ["incident_datetime", "incident_date", "incident_time", "incident_year", "incident_day_of_week",
-                 "report_datetime"]
-    location_cols = ["latitude", "longitude"]
-    incident_details_cols = ["incident_number", "incident_description"]
+        date_keys = bulk_insert_and_get_keys(session, batch_df, DateDimension)
+        incident_detail_keys = bulk_insert_and_get_keys(session, batch_df, IncidentDetailsDimension)
 
-    for start_idx in tqdm(range(0, total_rows, batch_size), desc="Inserting rows"):
-        end_idx = start_idx + batch_size
-        batch_df = df.iloc[start_idx:end_idx]
-
-        # Prepare values for batch insertion more efficiently
-        date_values = batch_df[date_cols].to_dict('records')
-        location_values = batch_df[location_cols].to_dict('records')
-        incident_details_values = batch_df[incident_details_cols].to_dict('records')
-
-        # Bulk insert
-        session.bulk_insert_mappings(class_mapper(DateDimension), date_values)
-        session.bulk_insert_mappings(class_mapper(LocationDimension), location_values)
-        session.bulk_insert_mappings(class_mapper(IncidentDetailsDimension), incident_details_values)
-
-        # Optimizing the way we get the IDs (one possible method)
-        last_date_key = session.query(DateDimension.date_key).order_by(DateDimension.date_key.desc()).first()[0]
-        date_keys = range(last_date_key - len(date_values) + 1, last_date_key + 1)
-        last_location_key = \
-        session.query(LocationDimension.location_key).order_by(LocationDimension.location_key.desc()).first()[0]
-        location_keys = range(last_location_key - len(location_values) + 1, last_location_key + 1)
-        last_incident_details_key = session.query(IncidentDetailsDimension.incident_details_key).order_by(
-            IncidentDetailsDimension.incident_details_key.desc()).first()[0]
-        incident_detail_keys = range(last_incident_details_key - len(incident_details_values) + 1,
-                                     last_incident_details_key + 1)
-
-        # Prepare the final batch values for insertion
         batch_values = [
             {
-                'row_id': row.row_id,
                 'date_key': date_keys[idx],
-                'category_key': mappings['category_dimension'][
-                    tuple([row.incident_category, row.incident_subcategory, row.incident_code])],
-                'district_key': mappings['district_dimension'][tuple([row.police_district, row.analysis_neighborhood])],
-                'resolution_key': mappings['resolution_dimension'][tuple([row.resolution])],
-                'location_key': location_keys[idx],
+                'category_key': get_key(row, mappings[CategoryDimension], *CategoryDimension.get_columns()),
+                'district_key': get_key(row, mappings[DistrictDimension], *DistrictDimension.get_columns()),
+                'resolution_key': get_key(row, mappings[ResolutionDimension], *ResolutionDimension.get_columns()),
+                'location_key': get_key(row, mappings[LocationDimension], *LocationDimension.get_columns()),
                 'incident_details_key': incident_detail_keys[idx]
             }
-
             for idx, row in enumerate(batch_df.itertuples(index=False))
         ]
 
-        session.bulk_insert_mappings(class_mapper(Incidents), batch_values)
-        # Close the session
+        session.bulk_insert_mappings(Incidents, batch_values)
+
     session.commit()
     session.close()
 
 
-def main(orm_exc=None):
+def main():
     """
-    Main function to execute the script.
+    Main function to execute the script. It sets up the database connection,
+    loads the data, and inserts it into the database.
+    :return: None
     """
-    # Define engine
     engine = create_engine(
         f"postgresql://{db_config['user']}:{db_config['password']}@{db_config['host']}:{db_config['port']}/{db_config['dbname']}")
 
-    # Reflect the tables and map them to the ORM classes
-    meta = MetaData()
-    meta.reflect(bind=engine)
-
-    # Use registry for mapping
-    mapper_registry = registry()
-
-    for class_name, table_name in [('DateDimension', 'date_dimension'),
-                                   ('LocationDimension', 'location_dimension'),
-                                   ('IncidentDetailsDimension', 'incident_details_dimension'),
-                                   ('DistrictDimension', 'district_dimension'),
-                                   ('ResolutionDimension', 'resolution_dimension'),
-                                   ('CategoryDimension', 'category_dimension'),
-                                   ('Incidents', 'incidents')
-                                   ]:
-        orm_class = globals()[class_name]
-        table = meta.tables[table_name]
-        try:
-            # Check if the class is already mapped
-            class_mapper(orm_class)
-        except orm_exc.UnmappedClassError:
-            # If not, map it
-            mapper_registry.map_imperatively(orm_class, table)
-
-    # Load data and insert it into the database
     df = load_data('data/crime_sf.csv')
     insert_data(df, engine)
 
--- model/SQLAlchemy.py
+++ model/SQLAlchemy.py
@@ -1,19 +1,31 @@
-from sqlalchemy.orm import sessionmaker
-import sqlalchemy
+from sqlalchemy.orm import declarative_base, DeclarativeMeta
 from sqlalchemy import Column, Integer, String, TIMESTAMP, DATE, TIME, FLOAT, ForeignKey, INT, TEXT
+from abc import abstractmethod, ABCMeta
 
-Base = sqlalchemy.orm.declarative_base()
+Base = declarative_base()
 
 
 def get_base():
     return Base
 
 
-class DateDimension(Base):
-    """Date Dimension Model"""
+class ABCWithSQLAlchemy(ABCMeta, DeclarativeMeta):
+    pass
+
+
+class Dimension(Base, metaclass=ABCWithSQLAlchemy):
+    __abstract__ = True
+    key = Column(Integer, primary_key=True, autoincrement=True)
+
+    @classmethod
+    @abstractmethod
+    def get_columns(cls):
+        pass
+
+
+class DateDimension(Dimension):
     __tablename__ = 'date_dimension'
 
-    date_key = Column(Integer, primary_key=True, autoincrement=True)
     incident_datetime = Column(TIMESTAMP, nullable=False)
     incident_date = Column(DATE, nullable=False)
     incident_time = Column(TIME, nullable=False)
@@ -21,54 +33,74 @@ class DateDimension(Base):
     incident_day_of_week = Column(String(255), nullable=False)
     report_datetime = Column(TIMESTAMP, nullable=False)
 
+    @classmethod
+    def get_columns(cls):
+        return ['incident_datetime', 'incident_date', 'incident_time',
+                'incident_year', 'incident_day_of_week', 'report_datetime']
 
-class CategoryDimension(Base):
+
+class CategoryDimension(Dimension):
     __tablename__ = 'category_dimension'
 
-    category_key = Column(Integer, primary_key=True, autoincrement=True)
     incident_category = Column(String(255))
     incident_subcategory = Column(String(255))
     incident_code = Column(Integer, nullable=False)
 
+    @classmethod
+    def get_columns(cls):
+        return ['incident_category', 'incident_subcategory', 'incident_code']
 
-class DistrictDimension(Base):
+
+class DistrictDimension(Dimension):
     __tablename__ = 'district_dimension'
 
-    district_key = Column(Integer, primary_key=True, autoincrement=True)
     police_district = Column(String(255))
     analysis_neighborhood = Column(String(255))
 
+    @classmethod
+    def get_columns(cls):
+        return ['police_district', 'analysis_neighborhood']
 
-class IncidentDetailsDimension(Base):
+
+class IncidentDetailsDimension(Dimension):
     __tablename__ = 'incident_details_dimension'
 
-    incident_details_key = Column(Integer, primary_key=True, autoincrement=True)
     incident_number = Column(INT, nullable=False)
     incident_description = Column(TEXT)
 
+    @classmethod
+    def get_columns(cls):
+        return ['incident_number', 'incident_description']
 
-class LocationDimension(Base):
+
+class LocationDimension(Dimension):
     __tablename__ = 'location_dimension'
 
-    location_key = Column(Integer, primary_key=True, autoincrement=True)
     latitude = Column(FLOAT)
     longitude = Column(FLOAT)
 
+    @classmethod
+    def get_columns(cls):
+        return ['latitude', 'longitude']
 
-class ResolutionDimension(Base):
+
+class ResolutionDimension(Dimension):
     __tablename__ = 'resolution_dimension'
 
-    resolution_key = Column(Integer, primary_key=True, autoincrement=True)
     resolution = Column(String(255), nullable=False)
+
+    @classmethod
+    def get_columns(cls):
+        return ['resolution']
 
 
 class Incidents(Base):
     __tablename__ = 'incidents'
 
-    incident_id = Column(sqlalchemy.types.BIGINT, primary_key=True, autoincrement=True)
-    date_key = Column(Integer, ForeignKey('date_dimension.date_key'))
-    category_key = Column(Integer, ForeignKey('category_dimension.category_key'))
-    district_key = Column(Integer, ForeignKey('district_dimension.district_key'))
-    resolution_key = Column(Integer, ForeignKey('resolution_dimension.resolution_key'))
-    location_key = Column(Integer, ForeignKey('location_dimension.location_key'))
-    incident_details_key = Column(Integer, ForeignKey('incident_details_dimension.incident_details_key'))
+    incident_id = Column(Integer, primary_key=True, autoincrement=True)
+    date_key = Column(Integer, ForeignKey('date_dimension.key'))
+    category_key = Column(Integer, ForeignKey('category_dimension.key'))
+    district_key = Column(Integer, ForeignKey('district_dimension.key'))
+    resolution_key = Column(Integer, ForeignKey('resolution_dimension.key'))
+    location_key = Column(Integer, ForeignKey('location_dimension.key'))
+    incident_details_key = Column(Integer, ForeignKey('incident_details_dimension.key'))
