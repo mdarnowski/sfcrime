f4be0be137d12555f308f08fd73396f7eec6c144
Zacer559 <zacer559@gmail.com>
2023-06-23 13:56:03 +0200

    Added comment to main.py to paste crime_sf.csv file before launching.

--- main.py
+++ main.py
@@ -2,6 +2,6 @@
 from board.MainApplication import create_app
 
 app = create_app()
-
+# BEFORE RUNNING THE APP, MAKE SURE TO PLACE crime_sf.csv IN THE data FOLDER
 if __name__ == '__main__':
     app.run(debug=True, port=8050)
