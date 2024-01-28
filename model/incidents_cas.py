3e9687fe4c0af70460091ab71b28e912901f31a9
mdarnowski <s18322@pjwstk.edu.pl>
2024-01-28 15:32:06 +0100

    casandra connection try

--- model/incidents_cas.py
+++ model/incidents_cas.py
@@ -1,11 +1,31 @@
+import os
+
+# Set environment variable for Cassandra cluster IPs
+os.environ['CLUSTER_IPS'] = '127.0.0.1'  # or your Docker host IP
+
 import corm
 from corm.models import CORMBase
 from datetime import datetime
-from uuid import uuid4
+import uuid
 
+# Define the keyspace name
+keyspace_name = 'sfcrime_keyspace'
+
+# Create a CQL script to create the keyspace
+create_keyspace_cql = f"""
+CREATE KEYSPACE IF NOT EXISTS {keyspace_name}
+WITH REPLICATION = {{ 'class' : 'SimpleStrategy', 'replication_factor' : 1 }};
+"""
+
+# Use cqlsh to execute the script and create the keyspace
+os.system(
+    f'docker run --rm --network cassandra -e CQLSH_HOST=cassandra -e CQLSH_PORT=9042 nuvo/docker-cqlsh -e "{create_keyspace_cql}"')
+
+
+# Define the table structure with UUID type for incident_id
 class IncidentDetails(CORMBase):
-    __keyspace__ = 'your_keyspace'
-    incident_id: uuid4
+    __keyspace__ = keyspace_name
+    incident_id: uuid.UUID
     incident_datetime: datetime
     incident_date: datetime
     incident_time: datetime
@@ -22,6 +42,7 @@ class IncidentDetails(CORMBase):
     latitude: float
     longitude: float
     resolution: str
+
 
 # Register and create the table
 corm.register_table(IncidentDetails)
