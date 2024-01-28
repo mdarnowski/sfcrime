366eb7b29cfe32e864fe108d0bf8bc94b1d2f502
Zacer559 <zacer559@gmail.com>
2024-01-28 16:07:51 +0100

    Wrong datapath for sfcrime

--- config/database.py
+++ config/database.py
@@ -2,7 +2,7 @@
     Database configuration
 """
 db_config = {
-    "contact_points": ["127.0.0.1"],  # IP address of your Docker Cassandra instance
+    "contact_points": ["192.168.1.104"],  # IP address of your Docker Cassandra instance
     "port": 9042,  # Default Cassandra port
     "keyspace": "sfcrime"  # Replace with your actual keyspace name
 }
