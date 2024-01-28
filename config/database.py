cda7c3804249f1e69a65385956aa93b55ec466bf
Zacer559 <zacer559@gmail.com>
2024-01-28 15:03:26 +0100

    Changed db config

--- config/database.py
+++ config/database.py
@@ -2,12 +2,8 @@
     Database configuration
 """
 db_config = {
-    "host": "127.0.0.1",
-    "port": "27017",
-    "dbname": "crime_data_sf",
-    # MongoDB doesn't require a username and password by default,
-    # but you can add them if you've set up authentication.
-    # "user": "<your_username>",
-    # "password": "<your_password>"
+    "contact_points": ["127.0.0.1"],  # IP address of your Docker Cassandra instance
+    "port": 9042,  # Default Cassandra port
+    "keyspace": "sfcrime"  # Replace with your actual keyspace name
 }
 
